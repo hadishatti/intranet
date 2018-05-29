d455eaf89e64a9cc81c9c1663d8d45ce7f226705
hadi <h.shatti@tecnositafgulf.qa>
2018-05-29 12:36:57 +0300

    removed target

--- target/intranet/js/ckconfig.js
+++
@@ -1,7 +0,0 @@
-CKEDITOR.editorConfig = function(config) {
-	config.toolbar = 'MyToolbar';
-
-	config.toolbar_MyToolbar = [ [ 'Preview' ],
-			[ 'Cut', 'Copy', 'Paste', 'PasteFromWord', '-' ],
-			[ 'Undo', 'Redo' ], [ 'Smiley' ], [ 'Bold', 'Italic' ], [ 'Link' ] ];
-};
